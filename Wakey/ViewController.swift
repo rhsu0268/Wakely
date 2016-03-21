5556f183b1e89be2f2eab20b61343a2025c2a84d
Richard Hsu <rhsu@gwu.edu>
2016-03-21 09:56:04 -0400

    Initial Commit

---
+++ Wakey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wakey
+//
+//  Created by Richard Hsu on 3/21/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
