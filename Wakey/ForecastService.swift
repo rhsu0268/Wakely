ad98166145e2dbfe7c88ddf600eff72dccd1a3c6
Richard Hsu <rhsu@gwu.edu>
2016-03-30 13:51:26 -0400

    Added ForecastService

---
+++ Wakey/ForecastService.swift
@@ -0,0 +1,64 @@
+//
+//  ForecastService.swift
+//  Wakey
+//
+//  Created by Richard Hsu on 3/30/16.
+//  Copyright © 2016 Richard Hsu. All rights reserved.
+//
+
+import Foundation
+
+struct ForecastService
+{
+    
+    let forecastAPIKey: String
+    let forecastBaseURL: NSURL?
+    
+    
+    init(APIKey: String)
+    {
+        self.forecastAPIKey = APIKey
+        forecastBaseURL = NSURL(string: "http://api.forecast.io/forecast/\(forecastAPIKey)/")
+    }
+    
+    func getForecast(lat: Double, long: Double, completion: (CurrentWeather? -> Void))
+    {
+        if let forecastURL = NSURL(string: "\(lat),\(long)", relativeToURL: forecastBaseURL)
+        {
+            let networkOperation = NetworkOperation(url: forecastURL)
+            
+            networkOperation.downloadJSONFromURL
+            {
+                (let JSONDictionary) in
+                let currentWeather = self.currentWeatherFromJSON(JSONDictionary)
+                completion(currentWeather)
+                
+                    
+            }
+        }
+        else
+        {
+            print("Could not construct a valid URL!")
+        }
+    }
+    /*
+    func currentWeatherFromJSON(jsonDictionary: [String]?) -> CurrentWeather?
+    {
+        if let currentWeatherDictionary = jsonDictionary?["currently"] as? [String: AnyObject] {
+            return CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        }
+        else {
+            print("JSON dictionary returned nil for 'currently' key")
+            return nil
+        }
+    }
+    */
+    func currentWeatherFromJSON(jsonDictionary: [String: AnyObject]?) -> CurrentWeather? {
+        if let currentWeatherDictionary = jsonDictionary?["currently"] as? [String:AnyObject] {
+            return CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        } else {
+            print("JSON dictionary returned nil for 'currently'")
+            return nil
+        }
+    }
+}
