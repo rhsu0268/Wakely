3580e8a60b905c8f8621115d4ad25746ccf2cea1
Richard Hsu <rhsu@gwu.edu>
2016-03-29 22:19:40 -0400

    Downloaded JSON data

--- Wakey/NetworkOperation.swift
+++ Wakey/NetworkOperation.swift
@@ -17,9 +17,48 @@ class NetworkOperation
     lazy var session: NSURLSession = NSURLSession(configuration: self.config)
     let queryURL: NSURL
     
+    typealias JSONDictionaryCompletion = ([String: AnyObject]?) -> Void
+    
     init(url: NSURL)
     {
         self.queryURL = url
+    }
+    
+    
+    
+    func downloadJSONFromURL(completion: JSONDictionaryCompletion)
+    {
+        let request: NSURLRequest = NSURLRequest(URL: queryURL)
+        let dataTask = session.dataTaskWithRequest(request)
+        {
+            (let data, let response, let error) in
+            
+            // 1. Check HTTP response for successful GET request
+            
+            if let httpResponse = response as? NSHTTPURLResponse
+            {
+                switch(httpResponse.statusCode)
+                {
+                    case 200:
+                        do {
+                            let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! [String: AnyObject]
+                            completion(jsonDictionary)
+                        } catch let error {
+                            print("JSON Serialization failed. Error: \(error)")
+                    }
+                    default:
+                        print("GET request not successful. HTTP status code: \(httpResponse.statusCode)")
+                }
+            }
+            else
+            {
+                print("Error: Not a valid HTTP response")
+            }
+            
+            
+        }
+        // Right after you create the task, you need to start it
+        dataTask.resume()
     }
     
 }
