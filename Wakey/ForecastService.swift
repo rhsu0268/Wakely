f2212db282b5893f77aa05f25621a910a3b12e73
Richard Hsu <rhsu@gwu.edu>
2016-04-02 17:17:10 -0400

    Added comments for NetworkOperation and ForecastService

--- Wakey/ForecastService.swift
+++ Wakey/ForecastService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// interact with the Forecast API
 struct ForecastService
 {
     
@@ -21,16 +22,23 @@ struct ForecastService
         forecastBaseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
     }
     
+    /// completion handler that accepts a closure
+    
     func getForecast(lat: Double, long: Double, completion: (CurrentWeather? -> Void))
     {
         if let forecastURL = NSURL(string: "\(lat),\(long)", relativeToURL: forecastBaseURL)
         {
+            // create a NetworkOperation instance
             let networkOperation = NetworkOperation(url: forecastURL)
             
+            // trailing closure
             networkOperation.downloadJSONFromURL
             {
+                // parse contents of dictionary and create populated instance of current weather
                 (let JSONDictionary) in
                 let currentWeather = self.currentWeatherFromJSON(JSONDictionary)
+                
+                // assign to completion handler - bubbles it up to viewcontroller
                 completion(currentWeather)
                 
                     
@@ -41,12 +49,23 @@ struct ForecastService
             print("Could not construct a valid URL!")
         }
     }
+    
+    // optional dictionary is passed in
+    // return type = option instance of CurrentWeather
 
     func currentWeatherFromJSON(jsonDictionary: [String: AnyObject]?) -> CurrentWeather? {
+        
+        // check that dictionary returns a non-nil for key currently
+        
+        // optional binding: make sure that the dictionary is non-nil value
+        // If it does this, we cast to dictionary type. 
+        // assign it to currentWeatherDictionary
         if let currentWeatherDictionary = jsonDictionary?["currently"] as? [String:AnyObject] {
+            
+            // return the currentWeatherDictionary as an argument of currentWeater struct 
             return CurrentWeather(weatherDictionary: currentWeatherDictionary)
         } else {
-            print("JSON dictionary returned nil for 'currently'")
+            print("JSON dictionary returned nil for 'currently' key")
             return nil
         }
     }
--- Wakey/NetworkOperation.swift
+++ Wakey/NetworkOperation.swift
@@ -29,19 +29,31 @@ class NetworkOperation
     func downloadJSONFromURL(completion: JSONDictionaryCompletion)
     {
         let request: NSURLRequest = NSURLRequest(URL: queryURL)
+        
+        // trailing closure since it's the last argument
         let dataTask = session.dataTaskWithRequest(request)
         {
+            // parameters of the closure
             (let data, let response, let error) in
             
             // 1. Check HTTP response for successful GET request
             
+            // cast response to NSHTTPURLResponse using optional binding since that's where the status code is
             if let httpResponse = response as? NSHTTPURLResponse
             {
                 switch(httpResponse.statusCode)
                 {
+                    // test for success - need to include other cases
                     case 200:
+                        // 2. create a JSON objectwith data
+                        // NSJSONSerlization
+                        // pass in data from the datatask
+                        // options: Specify the way data object is converted to JSON one
+                        // returns an anyobject type - cast it to [String: AnyObject]
                         do {
                             let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as! [String: AnyObject]
+                            
+                            // return the dictionary with the completion handler
                             completion(jsonDictionary)
                         } catch let error {
                             print("JSON Serialization failed. Error: \(error)")
