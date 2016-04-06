f7e81e1b779ceeb9aff16fb11f1400e022f0b6b9
Richard Hsu <rhsu@gwu.edu>
2016-04-06 16:08:17 -0400

    Added code for icons

--- Wakey/CurrentWeather.swift
+++ Wakey/CurrentWeather.swift
@@ -7,6 +7,60 @@
 //
 
 import Foundation
+import UIKit
+
+enum Icon: String
+{
+    // ten members for ten possible values
+    case ClearDay = "clear-day"
+    case ClearNight = "clear-night"
+    case Rain = "rain"
+    case Snow = "snow"
+    case Sleet = "sleet"
+    case Wind = "wind"
+    case Fog = "fog"
+    case Cloudy = "cloudy"
+    case PartlyCloudyDay = "partly-cloudy-day"
+    case PartlyCloudyNight = "partly-cloudy-night"
+    
+    // helper method
+    func toImage() -> UIImage?
+    {
+        var imageName: String
+        
+        
+        // match the value
+        // pass in rawValue. If it matches, we get value
+        // otherwise, it will not
+        switch self
+        {
+            case .ClearDay:
+                imageName = "clear-day.png"
+            case .ClearNight:
+                imageName = "clear-night.png"
+            case .Rain:
+                imageName = "rain.png"
+            case .Snow:
+                imageName = "snow.png"
+            case .Sleet:
+                imageName = "sleet.png"
+            case .Wind:
+                imageName = "wind.png"
+            case .Fog:
+                imageName = "fog.png"
+            case .Cloudy:
+                imageName = "cloudy.png"
+            case .PartlyCloudyDay:
+                imageName = "cloudy-day.png"
+            case .PartlyCloudyNight:
+                imageName = "cloudy-night.png"
+        }
+
+        
+        return UIImage(named: imageName)
+    }
+
+}
 
 struct CurrentWeather
 {
@@ -14,6 +68,7 @@ struct CurrentWeather
     let humidity: Int?
     let precipProbability: Int?
     let summary: String?
+    var icon: UIImage? = UIImage(named: "default.png")
     
     init(weatherDictionary: [String: AnyObject])
     {
@@ -36,5 +91,16 @@ struct CurrentWeather
         }
         summary = weatherDictionary["summary"] as? String
         
+        
+        if let iconString = weatherDictionary["icon"] as? String,
+            let weatherIcon: Icon = Icon(rawValue: iconString)
+            {
+                icon = weatherIcon.toImage()
+            }
+    
     }
-}
+    
+    
+    
+    
+   }
